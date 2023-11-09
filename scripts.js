83be12865face06fe468935925705c1c9a07251d
ghm7 <pozzisgnosis@outlook.com>
2023-11-09 00:41:01 -0300

    Add post function

--- scripts.js
+++ scripts.js
@@ -1,12 +1,60 @@
 const URL = 'https://654c35d877200d6ba8589e5d.mockapi.io';
 const ENDPOINT = '/users';
-const forms = Array.from(document.querySelectorAll('form'));
+const [getForm, postForm] = Array.from(document.querySelectorAll('form'));
 const resultTable = document.querySelector('#results');
 
-document.addEventListener('DOMContentLoaded', (e) => {
-  // Function get
-  const [getForm] = forms.filter((form) => form.id === 'get-box');
+// This function has two roles,
+// 1. Get the whole data when you don't provide the id
+// 2. Get the specific data by id
+const getData = async (url, endpoint, id) => {
+  if (id) {
+    return fetch(url + endpoint + `/${id}`)
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((error) => error);
+  }
 
+  return fetch(url + endpoint)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((error) => error);
+};
+
+// Creates a new entry in the database
+const postData = async (url, endpoint, sendData) => {
+  return fetch(url + endpoint, {
+    headers: { 'Content-Type': 'application/json; charset=utf-8' },
+    method: 'POST',
+    body: JSON.stringify(sendData),
+  })
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((error) => error);
+};
+
+// Renders the list
+const showList = (array) => {
+  resultTable.innerHTML = '';
+
+  array.forEach(({ id, name, lastName }) => {
+    id !== undefined
+      ? (resultTable.innerHTML += `
+          <li class="list-group-item bg-dark text-white">
+            <div>ID: ${id}</div>
+              <div>Name: ${name}</div>
+            <div>Last name: ${lastName}</div>
+          </li>
+      `)
+      : (resultTable.innerHTML += `
+          <li class="list-group-item bg-dark text-white">
+            <div>El valor ingresado no existe en la base de datos.</div>
+          </li>
+      `);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  // Get functionality
   getForm.addEventListener('submit', async (e) => {
     e.stopPropagation();
     e.preventDefault();
@@ -15,33 +63,27 @@ document.addEventListener('DOMContentLoaded', (e) => {
     let data = [];
 
     value !== ''
-      ? data.push(
-          await fetch(URL + ENDPOINT + `/${value}`)
-            .then((res) => res.json())
-            .then((data) => data)
-            .catch((error) => error)
-        )
-      : (data = await fetch(URL + ENDPOINT)
-          .then((res) => res.json())
-          .then((data) => data)
-          .catch((error) => error));
+      ? data.push(await getData(URL, ENDPOINT, value))
+      : (data = await getData(URL, ENDPOINT));
 
-    resultTable.innerHTML = '';
-
-    data.forEach(({ id, name, lastName }) => {
-      id !== undefined
-        ? (resultTable.innerHTML += `
-            <li class="list-group-item bg-dark text-white">
-              <div>ID: ${id}</div>
-                <div>Name: ${name}</div>
-              <div>Last name: ${lastName}</div>
-            </li>
-        `)
-        : (resultTable.innerHTML += `
-            <li class="list-group-item bg-dark text-white">
-              <div>El valor ingresado no existe en la base de datos.</div>
-            </li>
-        `);
-    });
+    showList(data);
   });
+
+  // Post functionality
+  postForm.addEventListener('submit', async (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    const [inputName, inputLastName] = Array.from(
+      postForm.querySelectorAll('input')
+    );
+
+    const userData = { name: inputName.value, lastName: inputLastName.value };
+    postData(URL, ENDPOINT, userData);
+
+    const data = await getData(URL, ENDPOINT);
+    showList(data);
+  });
+
+  //
 });
